40564defa0fdb1fb0648449221fb9eef78b0d74b
Matthew Sorna <36307222+sorna-matthew@users.noreply.github.com>
2018-02-09 18:29:02 -0500

    mas: added logic to toggle pressure regulator given the proper command '$NT'

--- gcoderead_comment.py
+++ gcoderead_comment.py
@@ -13,6 +13,7 @@ fd = 'C:/Users/lab/Desktop/Users/Matt/m2-python/trunk/comment_test.txt'
 #Create and define serial port parameters
 
 ser = serial.Serial('COM7', 115200, timeout = 100)
+nordson = serial.Serial('COM5', 115200)
 time.sleep(2) # Make sure to give it enough time to initialize
 
 print('Serial port initialized')
@@ -23,7 +24,10 @@ print('Beginning print')
 with open(fd, "r") as gcode:
     for line in gcode:
         if line[0:1] == '$':
-            print(line)
+            brk = line.split('$')
+            cmd = brk[1]
+            if cmd == 'NT':
+                nordson.write(b'\x02\x30\x38\x44\x49\x30\x42\x03') #If the command after the $ escape character is NT, toggle the nordson pressure regulator
         else:
             ser.write(str.encode('{}\r\n'.format(line))) #Reads in text file (GCode) line by line and sends commands to M2
             read = b''
