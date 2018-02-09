44d0d3fd25781619b3cf59510226e626706cd02f
Matthew Sorna <36307222+sorna-matthew@users.noreply.github.com>
2018-02-09 13:29:49 -0500

    mas: introduced version on gcoderead that allows for non-gcode text to be present in the text file. these commands, prefaced by '$' could be later used for external control commands in Python using other packages/serial controls

---
+++ gcoderead_comment.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Mon Feb  5 14:28:34 2018
+
+@author: Matthew Sorna
+"""
+
+import serial
+import time
+
+#fd = 'C:/Users/lab/Desktop/Users/Matt/m2-python/trunk/Bistable_Sorna_gcode.txt'
+fd = 'C:/Users/lab/Desktop/Users/Matt/comment_test.txt'
+
+#Create and define serial port parameters
+
+ser = serial.Serial('COM7', 115200, timeout = 100)
+time.sleep(2) # Make sure to give it enough time to initialize
+
+print('Serial port initialized')
+for x in range(21): # Reads in all 21 lines of initalization text for the M2
+    init = ser.readline()
+    #print(init)
+print('Beginning print')
+with open(fd, "r") as gcode:
+    for line in gcode:
+        if line[0:1] == '$':
+            print(line)
+        else:
+            ser.write(str.encode('{}\r\n'.format(line))) #Reads in text file (GCode) line by line and sends commands to M2
+            read = b''
+            while read[0:2] != b'ok': #Waits for printer to send 'ok' command before sending the next command, ensuring print accuracy
+                read = ser.readline()
+            
+print('Print complete!\nSerial port closed')
+ser.close() #Closes serial port
