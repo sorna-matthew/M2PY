7a7646bd0677a19b0b537d71e19674ca8bddb371
Matthew Sorna <36307222+sorna-matthew@users.noreply.github.com>
2018-02-14 10:56:12 -0500

    mas: updated file paths

--- gcoderead_comment.py
+++ gcoderead_comment.py
@@ -13,13 +13,13 @@ fd = 'C:/Users/lab/Desktop/Users/Matt/m2-python/trunk/comment_test.txt'
 #Create and define serial port parameters
 
 ser = serial.Serial('COM7', 115200, timeout = 100)
-#nordson = serial.Serial('COM5', 115200)
+nordson = serial.Serial('COM5', 115200)
 time.sleep(2) # Make sure to give it enough time to initialize
 
 print('Serial port initialized')
 for x in range(21): # Reads in all 21 lines of initalization text for the M2
     init = ser.readline()
-    #print(init)
+
 print('Beginning print')
 with open(fd, "r") as gcode:
     for line in gcode:
@@ -27,7 +27,7 @@ with open(fd, "r") as gcode:
             brk = line.split('$')
             cmd = brk[1]
             if cmd == 'NT':
-                nordson.write(b'\x02\x30\x38\x44\x49\x30\x42\x03') #If the command after the $ escape character is NT, toggle the nordson pressure regulator
+                nordson.write(b'\x05\x02\x30\x38\x44\x49\x30\x42\x03\x04') #If the command after the $ escape character is NT, toggle the nordson pressure regulator
         else:
             ser.write(str.encode('{}\r\n'.format(line))) #Reads in text file (GCode) line by line and sends commands to M2
             read = b''
