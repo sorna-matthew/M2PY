c99eb4ef8b9c858fa7045c070666549c059b984b
Matthew Sorna <36307222+sorna-matthew@users.noreply.github.com>
2018-04-12 11:07:56 -0400

    lk: added updated rotator print paths

--- print paths/rotator_m2py_code_pin_screwholes.py
+++ print paths/rotator_m2py_code_pin_screwholes.py
@@ -8,35 +8,43 @@ import numpy as np
 import m2py as mp
 import matplotlib.pyplot as plt
 
-mg = mp.mopen('COM6',115200)
-
-# Key geometric parameters (mm)
-L = 8
-ratio = 1.25
-R = ratio*L
-numLayers = 10 # Actual number of layers will be twice this!
-
-# Supporting geometric parameters (mm)
-b = 7           # "Buffer" to allow for R > L case. Also makes room for holding down the element during torsion testing.
-s = 4           # Outer support ring thickness
-w = 6           # Width of rotator blades
-p = 4.78 + 1.00  # Pin width + extrusion thickness at 42 mm/s
-
-dimsum = R + L 
-dimdiff = R - L
-edge = 2*b + 2*s + 2*R  # Outer edge length
-bdiff = b + dimdiff     # Distance between inner edges of support
-wdiff = R - w           # R
-beam = bdiff + L + w    # Total extrusion length of beam (for better length control)
+# mg = mp.mopen('COM6',115200)
 
 # Print parameters
-z0 = -169           # Starting z position, based on zeroing manually, mm
-dz = 0.52 * 0.250   # Layer height based on nozzle diameter, mm
-fs = 25             # Frame speed, mm/s
-bs = 15             # Beam speed, mm/s
+fs = 42             # Frame speed, mm/s
+bs = 35             # Beam speed, mm/s
+nozzle = 0.410      # Nozzle inner diameter, mm
+# -- (dependent/won't often change)
+dz = 0.52*nozzle    # Layer height based on nozzle diameter, mm
+exw = 1.00          # Approximate frame extrusion width, mm - this is overlarge to account for rounding/accumulation at corners.
+numLayers = 10      # Actual number of layers will be twice this!
+
+# Key geometric parameters, mm
+L = 12
+ratio = 1.25
+# -- (dependent)
+R = ratio*(L-exw)   # Since beam length starts from edge of frame extrusion, not center
+
+# Supporting geometric parameters, mm
+b = 7           # "Buffer" to allow for R > L case. Also makes room for holding down the element during torsion testing.
+s = 3           # Outer support ring thickness
+w = 6           # Width of rotator blades
+# -- (dependent)
+p = 4.78 + exw  # Pin width
+f = 3.18 + exw  # Screw hole width for fixture
+
+# Internal dimensions, mm (dependent)
+dimsum = R + L          # longest inner edge dimension on frame
+dimdiff = R - L         # Addition to buffer along length of beam (may be negative)
+edge = 2*b + 2*s + 2*R  # Outer edge length - largest dimension
+bdiff = b + dimdiff     # Inner edge of fram along length of beam
+wdiff = R - w           # Space left between rotator blade and beam to allow for buckling
+beam = bdiff + L + w    # Total extrusion length of beam
+mindim = 2.5            # Minimum width of any epoxy-filled area
 
 # Define print path array for one layer. Entries are in the form (x,y,z,channel) where the coordinates are relative and the channel command (-1,0,1 correspond to none,off,on respectively) is to be executed after the move command.
 # Note that this automatically inserts mp.move(mg, 0,0,0) commands in between consecutive channel commands, unless later down in the execution you overwrite this.
+# All sets of sections are printed counterclockwise from the top right.
 printPath = np.array([
         [0, 0, 0, 1],
         # Outer edge of support
@@ -44,8 +52,31 @@ printPath = np.array([
         [-edge, 0, 0, -1],
         [0, edge, 0, -1],
         [edge, 0, 0, 0],
+        # Move to screw holes
+        [-(s+b+dimdiff-mindim), -(s+b-mindim), 0, 1],
+        # Screwholes (square)
+        [f, 0, 0, -1],
+        [0, f, 0, -1],
+        [-f, 0, 0, -1],
+        [0, -f, 0, 0],
+        [-(dimsum + 2*mindim), -dimdiff, 0, 1],       
+        [0, f, 0, -1],
+        [-f, 0, 0, -1],
+        [0, -f, 0, -1],
+        [f, 0, 0, 0],
+        [dimdiff, -(dimsum + 2*mindim), 0, 1],    
+        [-f, 0, 0, -1],
+        [0, -f, 0, -1],
+        [f, 0, 0, -1],
+        [0, f, 0, 0],
+        [(dimsum + 2*mindim), dimdiff, 0, 1],       
+        [0, -f, 0, -1],
+        [f, 0, 0, -1],
+        [0, f, 0, -1],
+        [-f, 0, 0, 0],
         # Move to inner edge
-        [-(s+b+dimdiff), -(s+b), 0, 1],
+        [0, mindim + dimsum, 0, -1],
+        [-(mindim + dimdiff), 0, 0, 1],
         # Inner edges of support
         [0, b, 0, -1],
         [-dimsum, 0, 0, 0],
@@ -107,9 +138,18 @@ plt.gca().set_aspect('equal',adjustable='box')
 plt.draw()    
 plt.savefig('rotator_L%d_ratio%.2f_b%d_w%d.png' % (L,ratio,b,w))
 
-# Display any warnings relating to print viability at the end so that they are visible without scrolling
-if dimdiff > (b - 2): print("Warning: buffer will not resolve well!") # Check that diff is small enough compared to b so as to not give the printer a hard time
-if (L/2) > wdiff: print("Warning: interference between buckled beam and rotator blade!") # Check that (L/2) <= wdiff so that the buckled beam doesn't contact the adjacent rotator blade
+# Display print info and any warnings relating to print viability
+print()
+print('\nEssentials: L %dmm, ratio %.2f, bs %dmm/s, fs %dmm/s' % (L,ratio,bs,fs))
+print('Largest dimension: %dmm' % (edge))
+print()
+if dimdiff > (b - mindim): print("**WARNING: buffer will not resolve well!") # Check that diff is small enough compared to b so as to not give the printer a hard time
+if (L/2) > wdiff: print("**WARNING: interference between buckled beam and rotator blade!") # Check that (L/2) <= wdiff so that the buckled beam doesn't contact the adjacent rotator blade
+if (w-p/2) < mindim: print("**WARNING: distance between pin edge and rotator blade is too small!") # Check that (L/2) <= wdiff so that the buckled beam doesn't contact the adjacent rotator blade
+fixture = np.sqrt((dimsum + 2*mindim)**2 + dimdiff**2)
+if fixture < 18.8 or fixture > 36.5: print("**WARNING: won't fit in test fixture!") # For dimensions specified by Snehal
+print("Fixture-fitting size: %.2fmm" % (fixture))
+
 
 #%%
 
@@ -119,10 +159,10 @@ mp.coord(mg, coord = 'rel')
 curSpeed = fs
 mp.speed(mg, speed = curSpeed)    # mm/s
 mp.home(mg, axes = 'X Y Z')
-p0 = [63, 185, -152.66349]         # for use again below
+p0 = [99, 160, -176.664]         # for use again below
 mp.move(mg, x = p0[0], y = p0[1], z = p0[2]) # Moves to start position of print
 
-mp.wait(mg, seconds = 2) #Gives yourself time to adjust!
+mp.wait(mg, seconds = 5) #Gives yourself time to adjust!
 
 # Run print sequence as defined by printPath array
 mp.ch1on(mg)  # Initial extrusion of material before making key features helps with bed adhesion, still working on this!
